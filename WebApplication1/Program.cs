6e0dfe7717c2712b7cf685a155cccfc1fdd3d47d
vstoj <vstojadinovic.996@gmail.com>
2025-11-30 14:53:46 +0100

    cors allowed pre deployment

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .SetIsOriginAllowed(_ => true)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
