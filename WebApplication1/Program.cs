c36db305567a04569939abfbfe921fe5d8cffdc5
vstoj <vstojadinovic.996@gmail.com>
2025-11-30 14:58:29 +0100

    cs optimized

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,14 +1,14 @@
 ﻿using MapsterMapper;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Configuration;
 using WebApplication1.Middleware;
 using WebApplication1.Utils.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<TruckContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(TruckContext)))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddCors(options =>
@@ -16,55 +16,61 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .SetIsOriginAllowed(_ => true)
+            .SetIsOriginAllowed(_ => true)   
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
     });
 });
 
+//  MAPSTER
 var config = TypeAdapterConfig.GlobalSettings;
 MappingConfig.RegisterMappings();
-
 config.Default.IgnoreNullValues(true);
+
 builder.Services.AddSingleton(config);
 builder.Services.AddScoped<IMapper, ServiceMapper>();
 
+//  SERVICES
 builder.Services.AddControllers();
 builder.AddApiConfiguration();
 builder.Services.AddSwaggerConfiguration();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
 
-
 builder.Services.AddAuthentication("ManualJwtScheme")
     .AddScheme<AuthenticationSchemeOptions, ManualJwtHandler>("ManualJwtScheme", null);
-
 
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Swagger
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Truck API v1");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    // Production mode – Swagger exposed but no UI
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Truck API v1");
+        c.DocumentTitle = "Truck API Docs";
+    });
+}
 
 app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 
-// ErrorHandlerMiddleware MUST be first to catch all exceptions
 app.UseMiddleware<ErrorHandlerMiddleware>();
-
 app.UseMiddleware<JsonWebTokenMiddleware>();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-// RequestLoggingMiddleware should be after ErrorHandler to log responses
 app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.MapControllers();
