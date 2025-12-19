aa69b53a282bcb550ec8a38478ddf31776fbf3a5
vstoj <vstojadinovic.996@gmail.com>
2025-12-19 21:31:03 +0100

    update

--- WebApplication1/Utils/DTOs/TuraDTO/TuraReadDto.cs
+++ WebApplication1/Utils/DTOs/TuraDTO/TuraReadDto.cs
@@ -1,10 +1,7 @@
-﻿namespace WebApplication1.Utils.DTOs.TuraDTO;
-
-public class TuraReadDto
+﻿public class TuraReadDto
 {
     public int TuraId { get; set; }
-    public string RedniBroj { get; set; } = string.Empty;
-
+    public string? RedniBroj { get; set; }
     public string MestoUtovara { get; set; } = string.Empty;
     public string MestoIstovara { get; set; } = string.Empty;
 
@@ -15,23 +12,23 @@ public class TuraReadDto
 
     public string? KolicinaRobe { get; set; }
     public string? Tezina { get; set; }
-    public int VrstaNadogradnjeId { get; set; }
-    public string VrstaNadogradnjeNaziv { get; set; } = string.Empty;
-    public string? NapomenaKlijenta { get; set; }
 
     public decimal? UlaznaCena { get; set; }
     public decimal? IzlaznaCena { get; set; }
-    public string Valuta { get; set; } = "EUR";
 
-    public string StatusTure { get; set; } = string.Empty;
+    public string? Valuta { get; set; }
+    public string? StatusTure { get; set; }
+
+    public int? KlijentId { get; set; }
+    public int? PrevoznikId { get; set; }
+    public int? VoziloId { get; set; }
+    public int VrstaNadogradnjeId { get; set; }
+
     public bool KreiranPutniNalog { get; set; }
 
-    public int KlijentId { get; set; }
-    public string KlijentNaziv { get; set; } = string.Empty;
-
-    public int PrevoznikId { get; set; }
-    public string PrevoznikNaziv { get; set; } = string.Empty;
-
-    public int? VoziloId { get; set; }
+    // sve lookup nazive nullable
+    public string? PrevoznikNaziv { get; set; }
     public string? VoziloNaziv { get; set; }
+    public string? KlijentNaziv { get; set; }
+    public string? VrstaNadogradnjeNaziv { get; set; }
 }
