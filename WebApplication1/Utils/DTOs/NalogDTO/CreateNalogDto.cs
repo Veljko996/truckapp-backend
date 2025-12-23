88326924ecc7a865093335b05db224688ff0b814
vstoj <vstojadinovic.996@gmail.com>
2025-12-23 14:09:35 +0100

    dokumentacija

--- WebApplication1/Utils/DTOs/NalogDTO/CreateNalogDto.cs
+++ WebApplication1/Utils/DTOs/NalogDTO/CreateNalogDto.cs
@@ -3,5 +3,14 @@ namespace WebApplication1.Utils.DTOs.NalogDTO;
 public class CreateNalogDto
 {
     public int TuraId { get; set; }
+
+    public string? AdresaUtovara { get; set; }
+    public string? VrstaRobe { get; set; }
+    public string? Izvoznik { get; set; }
+    public string? Spedicija { get; set; }
+    public string? GranicniPrelaz { get; set; }
+    public string? Uvoznik { get; set; }
+    public string? UvoznoCarinjenje { get; set; }
+    public string? IzvoznoCarinjenje { get; set; }
 }
 
